34ae332486ca41f0c464909ef1ec08eb65eaa411
Buddhika sepali Rathnayaka <buddhirathnayaka@gmail.com>
2025-08-06 10:51:19 +0530

    Add Sepali_Intern_Software_Engineer.pdf

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -12,9 +12,9 @@ const Hero = () => {
         <p>Hi, I’m an Aspirig Software Developer passionate about coding and learning new technologies. This is my portfolio where I share my projects and skills. I’m looking for opportunities to grow and work on real-world software development.</p>
         <div className="hero-action">   
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <a href="/Sepali_Intern_Software_Engineer.pdf" download className="hero-resume">Download My Resume</a>
-     </div>
-     </div>
+        <a href="/Sepali_Intern_Software_Engineer.pdf" download="Sepali_Intern_Software_Engineer.pdf"className="hero-resume">Download My Resume</a>
+    </div>
+    </div>
   )
 }
 
